af8cba8748b141c9ccf2636d4f499802c340234b
BoonsNaibot <deguzouwa@yahoo.com>
2014-05-21 15:16:57 -0500

    Update weakreflist.pyx

--- kivy/weakreflist.pyx
+++ kivy/weakreflist.pyx
@@ -20,8 +20,8 @@ cdef class WeakList(list):
         return _get_object(super(WeakList, self).__getitem__(item))
 
     def __getslice__(self, int i, int j):
-        cdef list __getslice__ = super(WeakList, self).__getslice__(i, j)
-        return [_get_object(x) for x in __getslice__] #slow?
+        cdef list _slice = super(WeakList, self).__getslice__(i, j)
+        return [_get_object(x) for x in _slice] #slow?
 
     def __iter__(self):
         for x in super(WeakList, self).__iter__():
